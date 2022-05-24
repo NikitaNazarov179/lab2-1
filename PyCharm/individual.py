5e306493e6c1ab91d065f2577f25bb550f4f49d8
NikitaNazarov179 <99790237+NikitaNazarov179@users.noreply.github.com>
2022-05-25 00:45:18 +0300

    решения

---
+++ PyCharm/individual.py
@@ -0,0 +1,8 @@
+#19 variant
+print ("vvedite kolichestvo zhitelei")
+k = int(input())
+print ("vvdeite ploshad'")
+p = int(input())
+j = k/p
+print ("plotnost naselenia ravna")
+print (j)
---
+++ PyCharm/numbers.py
@@ -0,0 +1,9 @@
+print("Vvedite 4 chisla")
+p = int(input())
+k = int(input())
+j = int(input())
+i = int(input())
+n = k+p
+c = i+j
+z = n/c
+print(z)
