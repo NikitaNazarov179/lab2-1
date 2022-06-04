98147cf7162ce08fb626c38bb3374288f42155c8
NikitaNazarov179 <99790237+NikitaNazarov179@users.noreply.github.com>
2022-06-04 17:24:40 +0300

    zadaniya

---
+++ PyCharm/ariphmetic.py
@@ -0,0 +1,6 @@
+print("reshite primer 4*100-54")
+print ("vash otvet")
+p=int(input())
+print ("pravil'nii otvet:")
+k = 4*100-54
+print (k)
---
+++ PyCharm/user.py
@@ -0,0 +1,6 @@
+name = str(input("What's your name? "))
+age = int(input("How old are you? "))
+livesin = str(input("Where do you live? "))
+print("this is ",name)
+print("it is ",age)
+print("(S)he lives in ",livesin)
