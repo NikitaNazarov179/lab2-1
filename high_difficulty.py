edb518a477f76277cf4a6fa07c7b7a7a326832e6
NikitaNazarov179 <99790237+NikitaNazarov179@users.noreply.github.com>
2022-06-04 17:15:51 +0300

    asd

--- high_difficulty.py
+++ high_difficulty.py
@@ -1,9 +1,4 @@
-a2 = int(input('add a2 '))
-a1 = int(input('add a1 '))
-b2 = int(input('add b2 '))
-b1 = int(input('add b1 '))
 
-c2 = a2 + b2 + (a1 + b1) // 10
-c1 = (a1 + b1) % 10
-
-print(c2, c1)
+y = float(input('add y: '))
+print('hours: ', y // 30)
+print('minutes: ', y % 30 * 2)
